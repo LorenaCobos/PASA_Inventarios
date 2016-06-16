e6efa056a3e31ad886aa73702262329f1a5e0e36
LorenaCobos <lorena.lara.cobos@gmail.com>
2016-06-16 14:59:09 -0500

    captura de escanner

---
+++ app/src/main/java/pasa/inventarios/com/pasa_inventarios/Activity_CaptureBarCodeScanner.java
@@ -0,0 +1,14 @@
+package pasa.inventarios.com.pasa_inventarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_CaptureBarCodeScanner extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__capturebarcodescanner);
+    }
+}
